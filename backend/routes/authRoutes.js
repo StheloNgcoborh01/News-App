e6b051919269fc2839488b4d64ab7612a2bb3dcc
StheloNgcoborh01 <yebonganeza@gmail.com>
2025-11-03 13:04:53 +1400

    added the forgot password route and file

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,9 +4,13 @@ import { db } from "../config/db.js";
 import { Checkpassword } from "../utilis/helpers.js";
 import { generateCode, sendCode } from "../utilis/helpers.js";
 
+import dotenv from "dotenv";
+
 import bcrypt from "bcrypt";
 
 const router = express.Router();
+
+dotenv.config();
 
 router.post("/signUp", async (req, res) => {
   const { email, password, Confirmpassword } = req.body;
@@ -147,7 +151,6 @@ router.post("/verify", async (req, res) => {
         });
       }
 
-
     } catch (err) {
       console.error(err);
       res.render("verify", { error: ["Something went wrong. Try again."] });
@@ -156,6 +159,132 @@ router.post("/verify", async (req, res) => {
     res.render("verify", { error: ["Invalid verification code. Try again."] });
   }
 });
+
+router.get("/forgotverify",  (req, res) =>{
+
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  res.render("forgotverify");
+});
+
+router.get("/forgotPassword", async (req, res) =>{
+  
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  res.render("forgotPassword");
+
+});
+
+
+router.post("/forgotPassword", async (req, res) =>{
+
+ const { email} = req.body;
+
+ try {
+
+      const result = await db.query(
+      "SELECT * FROM userdetails WHERE email = $1 AND is_verified = true",
+      [email]
+    );
+
+    if (!(result.rows.length > 0)){
+   return res.render("forgotPassword", {
+      error: ["Email Does Not Exist in Our databse please try again"],
+    });
+    }
+
+    const code = generateCode();
+    await sendCode(email, code);
+
+    // Store ALL signup data in session temporarily
+    req.session.ForgotTemp = {
+      email: email,
+      verificationCode: code,
+    };
+
+    req.session.save((err) => {
+      if (err) {
+        console.error(err);
+        return res.render("forgotverify", {
+          error: ["Session error. Please try again."],
+        });
+      }
+      res.redirect("/auth/forgotverify");
+    });
+  
+ } catch (error) {
+  return res.render("forgotPassword", {
+      error: ["An error occurred. Please try again."],
+    });
+ }
+
+
+
+});
+
+router.post("/forgotverify", (req, res) =>{
+
+const { code} = req.body;
+ const tempUser = req.session.ForgotTemp;
+
+  if (!tempUser) {
+    return res.redirect("/auth/forgotverify");
+  }
+
+  if (code.trim() === tempUser.verificationCode){
+
+      return res.redirect("/auth/newpassword");
+
+  }
+
+  else{
+   return res.render("forgotverify", {
+      error: ["password do not match"],
+    });
+  }
+});
+
+router.get("/newpassword", (req, res) =>{
+
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  res.render("newpassword");
+
+});
+
+router.post("/newPassword", async (req, res) =>{
+  const {password} = req.body;
+  const tempUser = req.session.ForgotTemp;
+
+    if (!tempUser) {
+    return res.redirect("/auth/forgotPassword");
+  }
+
+  console.log(tempUser);
+
+  try {
+
+    const SaltRound = parseInt(process.env.SALT_ROUNDS)
+    const hash = await bcrypt.hash(password, SaltRound);
+
+      const result = await db.query("UPDATE userdetails SET password = $1, verification_code = NULL WHERE email = $2 RETURNING *", [hash, tempUser.email] );
+       
+          delete req.session.ForgotTemp;
+         req.login(result.rows[0], (err) => {
+          if (err) return next(err);
+          res.redirect("/");
+        });
+  } catch (error) {
+
+     return res.render("newPassword", {
+      error: ["Cannot Update Password"],
+    });
+  }
+});
+
 
 router.get("/logout", (req, res, next) => {
   req.logout(function (err) {

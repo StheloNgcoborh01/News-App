f382b0ec8a14fcf2aee7477d5c578dc259d83991
Asanda_Ngcobo <yebonganeza@gmail.com>
2026-02-15 05:31:52 +1400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,29 +1,14 @@
 import pg from "pg";
 import pgSession from "connect-pg-simple";
-// const { Pool } = pg;
+
+const { Pool } = pg;
 
 
-// export const db = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: 5432,
-//   ssl: { rejectUnauthorized: false }
-// });
-
-const pool = new Pool({
+export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }  
 });
 
-export const testConnection = async () => {
-  try {
-    await db.connect();
-    console.log("DB connected");
-    client.release();
-  } catch (err) {
-    console.error("DB connection failed:", err);
-  }
-};
+
 
 export const PgSessionStore = pgSession;
